a4c7a23a7c492c7e9f0341177b548e518c974c74
Mohit Kacha <kachamohit3@gmail.com>
2019-06-23 18:25:24 -0300

    Updated password changes

--- src/main/java/com/group4/carrental/controllers/UpdatePasswordController.java
+++ src/main/java/com/group4/carrental/controllers/UpdatePasswordController.java
@@ -12,41 +12,91 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
 
 @Controller
 public class UpdatePasswordController {
 
+    private static final String PASSWORD_ERROR = "Password must be atleast 8 characters long,\n" +
+            "must include atleast 1 upperCase and 1 LowerCase character,\n" +
+            "Password must include atleast one number and special character";
+    private static final String PASSWORD_NOTMATCHED_ERROR = "password does not match";
+    private static final String OLD_PASSWORD_ERROR = "Old Password does not matched";
+
     @Autowired
     IUpdatePasswordService  updatePasswordService;
+
 
     public  UpdatePasswordController(@Qualifier("UpdatePasswordService") IUpdatePasswordService updatePasswordService){
         this.updatePasswordService = updatePasswordService;
     }
 
 
-    @GetMapping("/update_password")
-    public String updatePassword(){
+    @GetMapping("/update-password")
+    public String updatePassword(HttpSession session){
+
+        int userId = 0;
+        try {
+           userId = (int) session.getAttribute("user_id");
+
+        }catch (NullPointerException e){
+            return  "redirect:/login";
+        }
+
         return  "updatepassword";
     }
+    @PostMapping(value ="/update-password" )
+    public String saveUserPassword(@ModelAttribute("password")Password password, HttpSession session,Model model) throws UnsupportedEncodingException {
 
+        int userId =0;
+        try {
+            userId = (int) session.getAttribute("user_id");
+        }catch (NullPointerException e){
+            return  "redirect:/login";
+        }
 
-    @PostMapping(value ="/update_password" )
-    public String saveUserPassword(@ModelAttribute("password")Password password, HttpSession session,Model model){
+        boolean isValidData = true;
 
-        ModelAndView modelAndView;
+        if(updatePasswordService.isPasswordNull(password.getNewPassword())){
+            isValidData = false;
+        }else if(!updatePasswordService.validatePassword(password.getNewPassword())){
+            isValidData = false;
+            model.addAttribute("newPasswordError", PASSWORD_ERROR);
 
-        boolean isPasswordValid = updatePasswordService.validateOldPassword("1",password);
+        }
 
-        //String isPasswordValid = updatePasswordService.validateOldPassword(String.valueOf(session.getAttribute("username")),password);
-        System.out.println("message" +isPasswordValid);
+        if(updatePasswordService.isPasswordNull(password.getConfirmPassword())){
+            isValidData = false;
+        }else if(!updatePasswordService.isPasswordMatch(password.getNewPassword(),password.getConfirmPassword())){
+            isValidData = false;
+        }
 
-        if (isPasswordValid) {
-            updatePasswordService.updatePassword(password);
+        boolean isPasswordValid = updatePasswordService.isOldPasswordValid(userId,password);
+
+        if(!isPasswordValid){
+            isValidData = false;
+            model.addAttribute("oldPassworderror",OLD_PASSWORD_ERROR);
+            System.out.println("password does not matched");
+        }
+
+        System.out.println("is valid = "+isValidData);
+        if(isValidData){
+
+            String newPassword = password.getNewPassword();
+            String encodeNewPassword = updatePasswordService.getEncodedString(newPassword);
+
+            String confirmPassword = password.getConfirmPassword();
+            String encodeConfirmPassword = updatePasswordService.getEncodedString(confirmPassword);
+
+            password.setNewPassword(encodeNewPassword);
+            password.setConfirmPassword(encodeConfirmPassword);
+            updatePasswordService.updatePassword(userId,password);
             return  "redirect:/carrent";
 
         }else {
             model.addAttribute("error", "Invalid old Password");
             return "updatepassword";
         }
+
     }
 }
