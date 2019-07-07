6d08c009a49261b93f04a9340cc6f08dd5119ed7
Mohit Kacha <kachamohit3@gmail.com>
2019-07-07 20:33:29 -0300

    Added test cases

---
+++ src/test/java/com/group4/carrental/service/UserBookedCarServiceTest.java
@@ -0,0 +1,40 @@
+package com.group4.carrental.service;
+
+import com.group4.carrental.dao.IUserBookedCarsDAO;
+import com.group4.carrental.dao.IUserListedCarsDAO;
+import com.group4.carrental.dao.UserBookedCarDAOMock;
+import com.group4.carrental.model.CarList;
+import com.group4.carrental.service.implementation.UserBookedCarService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class UserBookedCarServiceTest {
+
+    IUserBookedCarsDAO userBookedCarsDAO;
+    IUserBookedCarService userBookedCarService;
+
+    @Before
+    public void setUp(){
+        this.userBookedCarsDAO = new UserBookedCarDAOMock();
+        this.userBookedCarService = new UserBookedCarService(userBookedCarsDAO);
+    }
+
+    @Test
+    public void getBookedCarTest(){
+        int userId = 10;
+        ArrayList<CarList> listOfCar = userBookedCarsDAO.getBookedCars(userId);
+        boolean answer = listOfCar.size() == 2;
+        assertTrue(answer);
+
+        userId = 11;
+        listOfCar = userBookedCarsDAO.getBookedCars(userId);
+        answer = listOfCar.size() == 2;
+        assertFalse(answer);
+    }
+
+}
